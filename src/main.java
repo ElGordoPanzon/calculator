4ee59ca63f1288b326da248aeda69b0d106b74f7
jose jimenez <jimenjos000@warren.k12.in.us>
2024-08-19 13:46:19 -0400

    klhul

--- src/main.java
+++ src/main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        isLeapYear(-1600);
+        isLeapYear(1600);
+        isLeapYear(2017);
+        isLeapYear(2000);
+
+
+    }
+
+    private static boolean isLeapYear(int year){
+       if(year % 4 == 0) {
+           if (year % 100 == 0){
+               if (year % 400 == 0){
+
+               }else {
+                   return false;
+               }
+
+           }else {
+               return false;
+           }
+
+       } else  {
+           return false;
+       }
+
+
+    }
 }
